9a4485d7e17135c5755633618d9c721bd8bf1923
unknown <fkorinaputri@gmail.com>
2023-07-09 12:01:26 +0700

    update

---
+++ resources/views/succees.blade.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Site made with Mobirise Website Builder v5.6.13, https://mobirise.com -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="generator" content="Mobirise v5.6.13, mobirise.com" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:image:src" content="" />
+    <meta property="og:image" content="" />
+    <meta name="twitter:title" content="Home" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+    <link rel="shortcut icon" href="{{ asset('desain/assets/images/logokpid.png') }}" type="image/x-icon" />
+    <meta name="description" content="" />
+
+    <title>SIENDRA</title>
+    <link rel="stylesheet" href="{{ asset('desain/assets/web/assets/mobirise-icons2/mobirise2.css') }}" />
+    <link rel="stylesheet" href="{{ asset('desain/assets/bootstrap/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('desain/assets/bootstrap/css/bootstrap-grid.min.css') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('desain/assets/bootstrap/css/bootstrap-reboot.min.css"') }} />
+    <link rel="stylesheet"
+        href="{{ asset('desain/assets/dropdown/css/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('desain/assets/socicon/css/styles.css') }}" />
+    <link rel="stylesheet" href="{{ asset('desain/assets/theme/css/style.css') }}" />
+    <link rel="preload"
+        href="https://fonts.googleapis.com/css?family=Inter:100,200,300,400,500,600,700,800,900&display=swap"
+        as="style" onload="this.onload=null;this.rel='stylesheet'" />
+    <noscript>
+        <link rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Inter:100,200,300,400,500,600,700,800,900&display=swap" />
+    </noscript>
+    <link rel="preload" as="style" href="{{ asset('desain/assets/mobirise/css/mbr-additional.css') }}" />
+    <link rel="stylesheet" href="{{ asset('desain/assets/mobirise/css/mbr-additional.css') }}" type="text/css" />
+</head>
+
+<body>
+    <section data-bs-version="5.1" class="menu menu3 cid-sFAA5oUu2Y" once="menu" id="menu3-1">
+        <nav class="navbar navbar-dropdown navbar-expand-lg">
+            <div class="container">
+                <div class="navbar-brand">
+                    <span class="navbar-logo">
+                        <img src="{{ asset('desain/assets/images/logokpid.png') }}" alt="Mobirise Website Builder"
+                            style="height: 3rem" />
+                    </span>
+
+                    <span class="navbar-logo">
+                        <img src="{{ asset('desain/assets/images/endra.png') }}" alt="Mobirise Website Builder"
+                            style="height: 6rem" />
+                    </span>
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse"
+                    data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <div class="hamburger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
+                        <li class="nav-item">
+                            <a class="nav-link link text-black display-7" href="">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link link text-black text-primary display-7"
+                                href="index.html#image2-4">Tentang</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link link text-black display-7" href="#contacts3-0">Kontak</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <section data-bs-version="5.1" class="header1 cid-sFCAOqBTxa" id="header1-i">
+        <div class="mbr-overlay" style="opacity: 0.4; background-color: rgb(0, 0, 0)"></div>
+
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-8">
+                    <h1 class="mbr-section-title mbr-fonts-style mb-3 display-1">
+                        <strong>SELAMAT DATANG</strong>
+                    </h1>
+
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        <strong>Klik mulai untuk segera mengisi kuesioner</strong>&nbsp;
+                    </p>
+                    <div class="mbr-section-btn mt-3">
+                        <a class="btn btn-black display-4" href="{{ route('respondent_page') }}">Mulai</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="image2 cid-tI8WNGIm54" id="image2-4">
+        <div class="container">
+            <center>
+                <h3 class="mbr-section-title mbr-fonts-style mb-3 display-2">
+                    <strong>Tentang</strong>
+                </h3>
+            </center>
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-4">
+                    <div class="image-wrapper">
+                        <img src="{{ asset('desain/assets/images/1-1.png') }}" alt="Mobirise Website Builder') }}" />
+                    </div>
+                </div>
+                <div class="col-12 col-lg">
+                    <div class="text-wrapper">
+                        <h3 class="mbr-section-title mbr-fonts-style mb-3 display-2">
+                            <strong>KPID SUMSEL</strong>
+                        </h3>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Komisi Penyiaran Indonesia Daerah (KPID) Provinsi Sumatera
+                            Selatan<br />
+                            adalah sebuah lembaga independen di Indonesia yang kedudukannya
+                            setingkat dengan lembaga negara lainnya yang befungsi sebagai
+                            regulator penyelenggaraan penyiaran di Indonesia. KPID bergerak
+                            di bidang pegawasan dan mengatur kegiatan penyiaran di wilayah
+                            Sumatera Selatan. Salah satu tujuan dari KPID adalah untuk
+                            memastikan bahwa stasiun televisi dan radio di wilayah tersebut
+                            menyajikan program-program yang bermutu dan sesuai dengan
+                            nilai-nilai kebangsaan.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="image2 cid-tI8VlcXGah" id="image2-3">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-4">
+                    <div class="image-wrapper">
+                        <img src="{{ asset('desain/assets/images/2-1.png') }}" alt="Mobirise Website Builder" />
+                    </div>
+                </div>
+                <div class="col-12 col-lg">
+                    <div class="text-wrapper">
+                        <h3 class="mbr-section-title mbr-fonts-style mb-3 display-2">
+                            <strong>SIENDRA</strong>
+                        </h3>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            SIENDRA (Aplikasi Evaluasi Indeks Masyarakat) adalah aplikasi
+                            penilaian kuesioner untuk mengetahui kepuasan masyarakat
+                            terhadap KPID SUMSEL melalui website yang dapat diakses melalui
+                            smartphone ataupun perersonal komputer, sehingga dapat menjadi
+                            bahan evaluasi untuk meningkatkan kinerja pelayanan instansi.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="contacts3 map1 cid-tI8SiMXhEL" id="contacts3-0">
+        <div class="container">
+            <div class="mbr-section-head">
+                <h3 class="mbr-section-title mbr-fonts-style align-center mb-0 display-2">
+                    <strong>Kontak</strong>
+                </h3>
+            </div>
+            <div class="row justify-content-center mt-4">
+                <div class="card col-12 col-md-6">
+                    <div class="card-wrapper">
+                        <div class="image-wrapper">
+                            <span class="mbr-iconfont mobi-mbri-phone mobi-mbri"
+                                style="color: rgb(255, 255, 255); fill: rgb(255, 255, 255)"></span>
+                        </div>
+                        <div class="text-wrapper">
+                            <h6 class="card-title mbr-fonts-style mb-1 display-5">
+                                <strong>Telepon</strong>
+                            </h6>
+                            <p class="mbr-text mbr-fonts-style display-7" style="color: white">(0711)-357101</p>
+                        </div>
+                    </div>
+                    <div class="card-wrapper">
+                        <div class="image-wrapper">
+                            <span class="mbr-iconfont mobi-mbri-letter mobi-mbri"
+                                style="color: rgb(255, 255, 255); fill: rgb(255, 255, 255)"></span>
+                        </div>
+                        <div class="text-wrapper">
+                            <h6 class="card-title mbr-fonts-style mb-1 display-5">
+                                <strong>Sms Pengaduan</strong>
+                            </h6>
+
+                            <p class="mbr-text mbr-fonts-style display-7" style="color: white">081279922900</p>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="map-wrapper col-12 col-md-6">
+                    <div class="google-map">
+                        <iframe frameborder="0" style="border: 0"
+                            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDk89J4FSunMF33ruMVWJaJht_Ro0kvoXs&amp;q=Komisi Penyiaran Indonesia Daerah Sumsel"
+                            allowfullscreen=""></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <hr />
+    <section data-bs-version="5.1" class="footer3 cid-sFAOjz8nX7" once="footers" id="footer3-d">
+        <div class="container">
+            <div class="media-container-row align-center mbr-white">
+                <div class="row social-row">
+                    <div class="social-list align-right pb-2">
+                        <div class="soc-item">
+                            <a href="https://www.instagram.com/kpidsumsel/?hl=id" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"
+                                    style="color: rgb(0, 0, 0); fill: rgb(0, 0, 0)"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.facebook.com/kpid.sumsel.7?mibextid=ZbWKwL" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon"
+                                    style="color: rgb(0, 0, 0); fill: rgb(0, 0, 0)"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://twitter.com/kpid_sumsel?s=11" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-twitter socicon"
+                                    style="color: rgb(0, 0, 0); fill: rgb(0, 0, 0)"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="mailto:kpidsumsel2020@gmail.com" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-mail socicon"
+                                    style="color: rgb(0, 0, 0); fill: rgb(0, 0, 0)"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row row-copirayt">
+                    <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
+                        © Copyright by SIENDRA
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="display-7"
+        style="
+        padding: 0;
+        align-items: center;
+        justify-content: center;
+        flex-wrap: wrap;
+        align-content: center;
+        display: flex;
+        position: relative;
+        height: 0rem;
+      ">
+        <a href="https://mobiri.se/2871549"
+            style="
+          flex: 1 1;
+          height: 0rem;
+          position: absolute;
+          width: 100%;
+          z-index: 1;
+        "></a>
+
+        <a style="z-index: 1" href="https://mobirise.com/website-design-software.html"></a>
+    </section>
+    <script src="{{ asset('desain/assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('desain/assets/smoothscroll/smooth-scroll.js') }}"></script>
+    <script src="{{ asset('desain/assets/ytplayer/index.js') }}"></script>
+    <script src="{{ asset('desain/assets/dropdown/js/navbar-dropdown.js') }}"></script>
+    <script src="{{ asset('desain/assets/theme/js/script.js') }}"></script>
+</body>
+
+</html>
